55625e283a3c0761a4f32a0c16afbf353168ee9c
Rafael Soriano <rsoriano.insti@gmail.com>
2022-12-29 10:09:49 +0100

    2

--- script.js
+++ script.js
@@ -5,28 +5,32 @@ const audioElement = document.getElementById("audio");
 // VoiceRSS Javascript SDK (modificado por Jacinto)
 const VoiceRSS={speech:function(e){this._validate(e),this._request(e)},_validate:function(e){if(!e)throw"The settings are undefined";if(!e.key)throw"The API key is undefined";if(!e.src)throw"The text is undefined";if(!e.hl)throw"The language is undefined";if(e.c&&"auto"!=e.c.toLowerCase()){var a=!1;switch(e.c.toLowerCase()){case"mp3":a=(new Audio).canPlayType("audio/mpeg").replace("no","");break;case"wav":a=(new Audio).canPlayType("audio/wav").replace("no","");break;case"aac":a=(new Audio).canPlayType("audio/aac").replace("no","");break;case"ogg":a=(new Audio).canPlayType("audio/ogg").replace("no","");break;case"caf":a=(new Audio).canPlayType("audio/x-caf").replace("no","")}if(!a)throw"The browser does not support the audio codec "+e.c}},_request:function(e){var a=this._buildRequest(e),t=this._getXHR();t.onreadystatechange=function(){if(4==t.readyState&&200==t.status){if(0==t.responseText.indexOf("ERROR"))throw t.responseText;audioElement.src=t.responseText,audioElement.play()}},t.open("POST","https://api.voicerss.org/",!0),t.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=UTF-8"),t.send(a)},_buildRequest:function(e){var a=e.c&&"auto"!=e.c.toLowerCase()?e.c:this._detectCodec();return"key="+(e.key||"")+"&src="+(e.src||"")+"&hl="+(e.hl||"")+"&r="+(e.r||"")+"&c="+(a||"")+"&f="+(e.f||"")+"&ssml="+(e.ssml||"")+"&b64=true"},_detectCodec:function(){var e=new Audio;return e.canPlayType("audio/mpeg").replace("no","")?"mp3":e.canPlayType("audio/wav").replace("no","")?"wav":e.canPlayType("audio/aac").replace("no","")?"aac":e.canPlayType("audio/ogg").replace("no","")?"ogg":e.canPlayType("audio/x-caf").replace("no","")?"caf":""},_getXHR:function(){try{return new XMLHttpRequest}catch(e){}try{return new ActiveXObject("Msxml3.XMLHTTP")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.6.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.3.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP")}catch(e){}try{return new ActiveXObject("Microsoft.XMLHTTP")}catch(e){}throw"The browser does not support HTTP request"}};
 
+// Enable/Disable button
+function toggleButton() {
+    button.disabled = !button.disabled
+}
 
 // Mi clave personal obtenida en VoiceRSS https://www.voicerss.org/
 // const apiKey = b5f7aad534ad4d98a69fa51424f312b6;
 // http://api.voicerss.org/?key=b5f7aad534ad4d98a69fa51424f312b6&hl=en-us&src=Hello, world!
 
-// function test () {
-//     VoiceRSS.speech({
-//         key: 'b5f7aad534ad4d98a69fa51424f312b6',
-//         src: 'Hello, world!',
-//         hl: 'en-us',
-//         v: 'Linda',
-//         r: 0, 
-//         c: 'mp3',
-//         f: '44khz_16bit_stereo',
-//         ssml: false
-//     });
-// }
-
-// button.onclick = test;
+//Pass the joke to the speech to voice functionality (VoiceRSS)
+function tellMe(joke) {
+    VoiceRSS.speech({
+        key: 'b5f7aad534ad4d98a69fa51424f312b6',
+        src: joke,
+        hl: 'en-us',
+        v: 'Linda',
+        r: 0,
+        c: 'mp3',
+        f: '44khz_16bit_stereo',
+        ssml: false
+    });
+}
 
 // Get jokes from Joke API
 async function getJokes() {
+    toggleButton();
     let joke = "";
     const apiURL = "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
     try {
@@ -37,10 +41,12 @@ async function getJokes() {
         }else {
             joke = data.joke
         }
-        console.log(joke)
+        // Send joke to speech to voice funcion
+        tellMe(joke)
     }catch (error) {
         console.log("Error al obtener un chiste. ", error)
     }
 }
 
+audioElement.addEventListener("ended", toggleButton);
 button.onclick = getJokes;
